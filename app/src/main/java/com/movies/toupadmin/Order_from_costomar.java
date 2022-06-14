d552ed184ab7fc1edd074e996c0e92905d5bf297
Shahain <shahinrhaman30@gmail.com>
2022-06-14 17:34:33 +0600

    add pushnoticatin

--- app/src/main/java/com/movies/toupadmin/Order_from_costomar.java
+++ app/src/main/java/com/movies/toupadmin/Order_from_costomar.java
@@ -6,7 +6,16 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
@@ -14,18 +23,19 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.movies.toupadmin.pushnotication.FcmNotificationsSender;
+
 
 import java.util.ArrayList;
+
 
 public class Order_from_costomar extends AppCompatActivity {
    public static RecyclerView recyclerView;
     public static   DatabaseReference reference;
     public static  AdminAdapter adapter;
     public static ArrayList<OrderData> list;
-
     private SwipeRefreshLayout swipeRefreshLayout;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +44,9 @@ public class Order_from_costomar extends AppCompatActivity {
         reference= FirebaseDatabase.getInstance().getReference().child("order");
         swipeRefreshLayout=findViewById(R.id.swift);
         list=new ArrayList<>();
-
-        loadData();
-
-        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+       FirebaseMessaging.getInstance().subscribeToTopic("notification");
+       loadData();
+       swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
 
@@ -55,7 +64,7 @@ public class Order_from_costomar extends AppCompatActivity {
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
-        adapter=new AdminAdapter(getApplicationContext(),list);
+        adapter=new AdminAdapter(this,list);
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
@@ -93,12 +102,154 @@ public class Order_from_costomar extends AppCompatActivity {
 
 
 
+
     }
 
 
 
 
+public class AdminAdapter extends RecyclerView.Adapter<AdminAdapter.ViewHolder>  {
 
+    Context context;
+    ArrayList<OrderData>list;
+
+
+    public AdminAdapter(Context context, ArrayList<OrderData> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(context).inflate(R.layout.order_item,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @SuppressLint("ResourceAsColor")
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+
+        OrderData data=list.get(position);
+
+
+        holder.ordername.setText(data.getFullname());
+        holder.gameId.setText(data.getPlayerid());
+        holder.packname.setText(data.getOrder_no());
+        holder.paymnetaccount.setText(data.getAccount_no());
+        holder.tracnsctionId.setText(data.getTransctionid());
+        holder.orderdate.setText(data.getDate());
+        holder.payment.setText(data.getBank());
+        FirebaseMessaging.getInstance().subscribeToTopic("all");
+
+        if (data.getColorcode().contains("f")){
+
+
+        }else {
+            holder.layout.setBackgroundColor(R.color.done);
+        }
+
+
+
+        holder.delevery.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+              ///  String massage="New massage";
+                String massage=data.getFullname().toString();
+                String titile="Delevery Done";
+
+                FcmNotificationsSender notificationsSender=new FcmNotificationsSender("/topics/all",
+                        titile,massage,getApplicationContext(),Order_from_costomar.this);
+
+                notificationsSender.SendNotifications();
+
+
+
+
+
+
+
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("order").child(data.getOrder_no()).child("colorcode");
+                ref.setValue("t");
+
+
+
+            }
+
+
+
+        });
+        holder.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+                DatabaseReference ref = FirebaseDatabase.getInstance().getReference("order").child(data.getOrder_no());
+
+                if (ref.equals(null)){
+                    Toast.makeText(context, "Data Is delete", Toast.LENGTH_SHORT).show();
+                }else ref.removeValue();
+                loadData();
+
+
+
+
+
+
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+
+
+        return list.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView ordername,gameId,packname,paymnetaccount,tracnsctionId,orderdate,payment;
+        LinearLayout layout;
+        Button button,delevery;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+            orderdate=itemView.findViewById(R.id.orderDate);
+            ordername=itemView.findViewById(R.id.ordername);
+            gameId=itemView.findViewById(R.id.gameuid);
+            packname=itemView.findViewById(R.id.packname);
+            paymnetaccount=itemView.findViewById(R.id.payment_account);
+            tracnsctionId=itemView.findViewById(R.id.transctionId);
+            payment=itemView.findViewById(R.id.payment_methord);
+            layout=itemView.findViewById(R.id.layout);
+            button=itemView.findViewById(R.id.delete);
+            delevery=itemView.findViewById(R.id.delevery);
+
+
+
+
+
+        }
+    }
+}
 
 
 
